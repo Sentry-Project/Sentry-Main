a63fe4246204074da7c68955b1013563dad0e359
wrq77 <74533971+wrq77@users.noreply.github.com>
2021-01-18 13:42:33 +0100

    dashboard page

--- app/models/Sensor.php
+++ app/models/Sensor.php
@@ -18,6 +18,175 @@
         return $results;    
     }
 
+   // get sensor information
+  public function getsensor($id){
+    $this->db->query("select sensor_data.SENSOR_VALUE, room.ROOM_NAME, sensor.SENSOR_NAME
+      from sensor_data,sensor,room,user
+      where sensor.SENSOR_ID=sensor_data.FK_SENSOR_ID 
+      and sensor.FK_ROOM_ID=room.ROOM_ID
+      and user.USER_ID= room.FK_USER_ID
+      and user.USER_ID= :id
+      ORDER by sensor_data.TIMESTAMP DESC
+      LIMIT 3"); 
 
+        $this->db->bind(':id', $id);
+        $row = $this->db->resultSet();
+
+        if($this->db->rowCount()>0){
+           return $row;
+       }else{
+           return false;
+       }
+  }
+
+  //when the danger level is high,turn off all the devices
+     public function offdev($id){
+     $this->db->query("update device,room,user set device.DEVICE_STATUS= '0' 
+      Where  device.FK_ROOM_ID = room.ROOM_ID
+      and user.USER_ID= room.FK_USER_ID
+      and user.USER_ID= :id ");
+
+      $this->db->bind(':id', $id);
+       if($this->db->execute()){
+           return true;
+         } else {
+        return false;
+         }
+      }
+
+     //get the average temperature of the house(whole rooms)
+    public function getAVETem($id){
+      $this->db->query("select round(AVG(sensor_data.SENSOR_VALUE),2) as ave_tem
+      from sensor_data,sensor,room,user
+      Where sensor_data.FK_SENSOR_ID = sensor.SENSOR_ID 
+      and sensor.FK_ROOM_ID = room.ROOM_ID
+      and user.USER_ID= room.FK_USER_ID
+      and sensor.SENSOR_NAME = 'temperature' 
+      and user.USER_ID= :id");
+
+      $this->db->bind(':id', $id);
+        $row = $this->db->single();
+
+        if($this->db->rowCount()>0){
+           return $row;
+       }else{
+           return false;
+       }
+    }
+
+        //get the gas reading(co concentration) of the kichen
+        public function getgas($id){
+          $this->db->query( "select sensor_data.SENSOR_VALUE as kitchen_gas
+          from sensor_data,sensor,room,user
+          where sensor.SENSOR_ID=sensor_data.FK_SENSOR_ID 
+          and sensor.FK_ROOM_ID=room.ROOM_ID
+          and user.USER_ID= room.FK_USER_ID
+          and sensor.SENSOR_NAME='co'
+          and user.USER_ID= :id");
+          $this->db->bind(':id', $id);
+          $row = $this->db->single();
+
+            if($this->db->rowCount()>0){
+               return $row;
+           }else{
+               return false;
+           }
+        }
+
+        //get the water_level of the kitchen
+        public function getwater($id){
+          $this->db->query("select sensor_data.SENSOR_VALUE as water_level
+          from sensor_data,sensor,room,user
+          Where sensor_data.FK_SENSOR_ID = sensor.SENSOR_ID 
+          and sensor.FK_ROOM_ID = room.ROOM_ID
+          and user.USER_ID= room.FK_USER_ID
+          and sensor.SENSOR_NAME='water' 
+          and user.USER_ID=:id ");
+          $this->db->bind(':id', $id);
+          $row = $this->db->single();
+
+            if($this->db->rowCount()>0){
+               return $row;
+           }else{
+               return false;
+           }
+        }
+
+        public function getondevice_num($id){
+          $this->db->query("select COUNT(DEVICE_ID) as On_device
+          from device,room,user
+          Where  device.FK_ROOM_ID = room.ROOM_ID
+          and user.USER_ID= room.FK_USER_ID
+          and DEVICE_STATUS=1 
+          and user.USER_ID=:id ");
+
+          $this->db->bind(':id', $id);
+          $row = $this->db->single();
+
+            if($this->db->rowCount()>0){
+               return $row;
+           }else{
+               return false;
+           }
+        }
+
+        //get the number of inactive device
+        public function getoffdevice_num($id){
+          $this->db->query("select COUNT(DEVICE_ID) as Off_device
+          from device,room,user
+          Where  device.FK_ROOM_ID = room.ROOM_ID
+          and user.USER_ID= room.FK_USER_ID
+          and DEVICE_STATUS=0 
+          and user.USER_ID=:id ");
+
+          $this->db->bind(':id', $id);
+          $row = $this->db->single();
+
+            if($this->db->rowCount()>0){
+               return $row;
+           }else{
+               return false;
+           }
+        }
+
+        //get the number of active sensor
+        public function getonsensor_num($id){
+          $this->db->query("select COUNT(ID) as On_sensor
+          from sensor_data,sensor,room,user
+          Where sensor_data.FK_SENSOR_ID = sensor.SENSOR_ID 
+          and sensor.FK_ROOM_ID = room.ROOM_ID
+          and user.USER_ID= room.FK_USER_ID
+          and SENSOR_STATUS=1 
+          and user.USER_ID=:id ");
+
+          $this->db->bind(':id', $id);
+          $row = $this->db->single();
+
+            if($this->db->rowCount()>0){
+               return $row;
+           }else{
+               return false;
+           }
+        }
+
+        //get the number of inactive sensor
+        public function getoffsensor_num($id){
+          $this->db->query("select COUNT(ID) as Off_sensor
+          from sensor_data,sensor,room,user
+          Where sensor_data.FK_SENSOR_ID = sensor.SENSOR_ID 
+          and sensor.FK_ROOM_ID = room.ROOM_ID
+          and user.USER_ID= room.FK_USER_ID
+          and SENSOR_STATUS=0 
+          and user.USER_ID= :id ");
+          
+          $this->db->bind(':id', $id);
+          $row = $this->db->single();
+
+            if($this->db->rowCount()>0){
+               return $row;
+           }else{
+               return false;
+           }
+        }
 
 }
--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -8,36 +8,113 @@
    <section id="mysentry">
     <div class="mysentry_heading">My Sentry</div>
     <div class="Alert_heading">Alert</div>
-  <!--alert-->
-   <div class="alert_container"> 
-     <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x"></i></div>
- 
-     <div class="intro">
-        <ul class="list">
-        <li>Low battery smoke detector</li>
-        <li>Room: living room</li>
-        <li>Level: low</li>
-        <li>Devices on: 0</li>
-        </ul>
-     </div>
-   </div>
+    <div class="showalert">
 
-     </br>
-     <div class="alert_container"> 
-     <div class="photo_2"><i class="fas fa-exclamation-circle fa-3x"></i></div>
+
+
+  <!--alert-->
+   <?php if($data['warning']):?>
+   <?php foreach($data['warning'] as $warning):?>
+    <?php  if($warning->SENSOR_NAME == 'temperature'and $warning->SENSOR_VALUE >= 45):?>
+    <div class="alert_container"> 
+    <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x" style="color: red"></i></div>
      <div class="intro">
         <ul class="list">
-        <li>Smoke detectoed</li>
-        <li>Room: kitchen</li>
-        <li>Level: high</li>
-        <li>Devices on: 3</li>
+        <li><?php echo $warning->SENSOR_NAME ?> detected</li>
+        <li>Room: <?php echo $warning->ROOM_NAME ?></li>
+        <li>Value: <?php echo $warning->SENSOR_VALUE ?></li>
+        <li> Danger: Over high temperature</li>
         </ul>
      </div>
-   </div>
+    </div>
+  <?php elseif ($warning->SENSOR_NAME == 'co' and $warning->SENSOR_VALUE >= 30 and $warning->SENSOR_VALUE <= 35):?>
+    <div class="alert_container"> 
+    <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x" style="color: green"></i></div>
+     <div class="intro">
+        <ul class="list">
+        <li><?php echo $warning->SENSOR_NAME ?> detected</li>
+        <li>Room: <?php echo $warning->ROOM_NAME ?></li>
+        <li>Value: <?php echo $warning->SENSOR_VALUE ?></li>
+        <li>Danger level: low</li>
+        </ul>
+     </div>
+    </div>
+
+   <?php elseif ($warning->SENSOR_NAME == 'co' and $warning->SENSOR_VALUE > 35 and $warning->SENSOR_VALUE <= 45):?>
+    <div class="alert_container"> 
+    <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x" style="color: yellow"></i></div>
+     <div class="intro">
+        <ul class="list">
+        <li><?php echo $warning->SENSOR_NAME ?> detected</li>
+        <li>Room: <?php echo $warning->ROOM_NAME ?></li>
+        <li>Value: <?php echo $warning->SENSOR_VALUE ?></li>
+        <li> Danger level: middle</li>
+        </ul>
+     </div>
+    </div>
+    <?php elseif ($warning->SENSOR_NAME == 'co' and $warning->SENSOR_VALUE >45):?>
+    <div class="alert_container"> 
+    <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x" style="color: red"></i></div>
+     <div class="intro">
+        <ul class="list">
+        <li><?php echo $warning->SENSOR_NAME ?> detected</li>
+        <li>Room: <?php echo $warning->ROOM_NAME ?></li>
+        <li>Value: <?php echo $warning->SENSOR_VALUE ?></li>
+        <li> Danger level: high</li>
+        </ul>
+     </div>
+    </div>
+    <?php elseif ($warning->SENSOR_NAME == 'water' and $warning->SENSOR_VALUE > 0.0008 and $warning->SENSOR_VALUE< 0.002):?>
+    <div class="alert_container"> 
+    <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x" style="color: green"></i></div>
+     <div class="intro">
+        <ul class="list">
+        <li><?php echo $warning->SENSOR_NAME ?> detected</li>
+        <li>Room: <?php echo $warning->ROOM_NAME ?></li>
+        <li>Value: <?php echo $warning->SENSOR_VALUE ?></li>
+        <li> Danger level: low</li>
+        </ul>
+     </div>
+    </div>
+    <?php elseif ($warning->SENSOR_NAME == 'water' and $warning->SENSOR_VALUE >= 0.002 and $warning->SENSOR_VALUE< 0.005):?>
+    <div class="alert_container"> 
+    <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x" style="color: yellow"></i></div>
+     <div class="intro">
+        <ul class="list">
+        <li><?php echo $warning->SENSOR_NAME ?> detected</li>
+        <li>Room: <?php echo $warning->ROOM_NAME ?></li>
+        <li>Value: <?php echo $warning->SENSOR_VALUE ?></li>
+        <li> Danger level: middle</li>
+        </ul>
+     </div>
+    </div>
+    <?php elseif ($warning->SENSOR_NAME == 'water' and $warning->SENSOR_VALUE >= 0.005):?>
+    <div class="alert_container"> 
+    <div class="photo_1"><i class="fas fa-exclamation-circle fa-3x" style="color: red"></i></div>
+     <div class="intro">
+        <ul class="list">
+        <li><?php echo $warning->SENSOR_NAME ?> detected</li>
+        <li>Room: <?php echo $warning->ROOM_NAME ?></li>
+        <li>Value: <?php echo $warning->SENSOR_VALUE ?></li>
+        <li> Danger level: high</li>
+        </ul>
+     </div>
+    </div>
+  <?php endif;?>
+   <?php endforeach; ?>
+   <?php else:?>
+    <p class="safe">no warnings now</p> 
+<?php endif; ?>
+<?php if($data['shownote']): ?>
+    <p class="note"> All devices automatically switched off</p>
+    <?php else:?>
+    <p class="safe"></p> 
+<?php endif;?>
+ </div>
+
  </section>
 
 
-   <p class="note"> All devices automatically switched off</p>
 
 
    <section id="overview">
@@ -48,40 +125,36 @@
     <div class="dashboardcard"> 
     <i class="fas fa-temperature-low fa-3x"></i>
     <div class="desc">Temperature</div>
-    <div class="desc_value">24 Celsius</div>
+    <div class="desc_value"><?php echo (!empty($data['avgtemp']->ave_tem) ? $data['avgtemp']->ave_tem : '0');?> Celsius</div>
     </div>
    
     <div class="dashboardcard"> 
     <i class="fas fa-wind fa-3x"></i>
     <div class="desc">Gas reading</div>
-    <div class="desc_value">27 mpg</div>
+    <div class="desc_value"><?php echo (!empty($data['gas']->kitchen_gas) ? $data['gas']->kitchen_gas : '0');?>ppm</div>
     </div>
 
      <div class="dashboardcard">
     <i class="fas fa-tint fa-3x"></i>
     <div class="desc">Water level</div>
-    <div class="desc_value">10 m3</div>
+    <div class="desc_value"><?php echo (!empty($data['waterlevel']->water_level) ? $data['waterlevel']->water_level : '0');?> m3</div>
     </div>
 
 
     <div class="dashboardcard">
     <div class="desc">Devices</div>
-    <div class="conte">Active: 5</div>
-    <div class="conte">Inactive: 0</div>
-    <div class="conte">Error: 0</div>
+    <div class="conte">Active: <?php echo (!empty($data['ondevice']->On_device) ? $data['ondevice']->On_device : '0');?></div>
+    <div class="conte">Inactive: <?php echo (!empty($data['offdevice']->Off_device) ? $data['offdevice']->Off_device : '0');?></div>
     </div>
 
     <div class="dashboardcard">
     <div class="desc">Sensors</div>
-    <div class="conte">Active: 5</div>
-    <div class="conte">Inactive: 0</div>
-    <div class="conte">Error: 0</div>
+    <div class="conte">Active: <?php echo (!empty($data['onsensor']->On_sensor) ? $data['onsensor']->On_sensor: '0');?></div>
+    <div class="conte">Inactive: <?php echo (!empty($data['offsensor']->Off_sensor) ? $data['offsensor']->Off_sensor: '0');?></div>
     </div> 
   </div>
 </div>
 </section>
 </br>
 </div>
-
-
 <?php require APPROOT . '/views/inc/footer.php'; ?>
